ad69583b99a1f99c4c4a66a5fb71c67ca6e9140c
Thomas <mosset.thomas@gmail.com>
2025-04-26 16:41:03 +0200

    back - added routes

---
+++ back/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, IncomeViewSet, ExpenseViewSet, BudgetViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet) # -> 'http://localhost:8000/api/categories/'
+router.register(r'incomes', IncomeViewSet) # -> '/api/incomes/'
+router.register(r'expenses', ExpenseViewSet) # -> '/api/expenses/'
+router.register(r'budgets', BudgetViewSet) # -> '/api/budgets/'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
