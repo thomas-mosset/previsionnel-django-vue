b5ca5356996d7582dde988945c8eef73c3e4c753
Thomas <mosset.thomas@gmail.com>
2025-04-26 15:48:22 +0200

    back - created ViewSets (Category, Income, Expense, Budget)

--- back/core/views.py
+++ back/core/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Income, Expense, Budget
+from .serializers import CategorySerializer, IncomeSerializer, ExpenseSerializer, BudgetSerializer
 
-# Create your views here.
+# each class will manage all actions (GET, POST, PUT, DELETE)
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all() # will need to work with all existing categories
+    serializer_class = CategorySerializer # which serializer to use to convert python object into JSON
+
+
+class IncomeViewSet(viewsets.ModelViewSet):
+    queryset = Income.objects.all()
+    serializer_class = IncomeSerializer
+
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+
+
+class BudgetViewSet(viewsets.ModelViewSet):
+    queryset = Budget.objects.all()
+    serializer_class = BudgetSerializer
