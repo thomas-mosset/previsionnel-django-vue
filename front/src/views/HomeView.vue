609f6adc73dc622dc06bc0090702b2f8d802311f
Thomas <mosset.thomas@gmail.com>
2025-04-27 20:31:31 +0200

    front - config vuetify

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>This is the home page</h1>
   </main>
 </template>
