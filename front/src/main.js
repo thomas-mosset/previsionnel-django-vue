57aaa090175f4dd82c74158f7bac0ca505cef371
Thomas <mosset.thomas@gmail.com>
2025-05-01 20:20:01 +0200

    front - profil added 4 cards (revenus, dépenses, budget, catégories)

--- front/src/main.js
+++ front/src/main.js
@@ -6,6 +6,7 @@ import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
 import router from './router'
--- front/src/views/ProfilView.vue
+++ front/src/views/ProfilView.vue
@@ -1,26 +1,53 @@
 <template>
   <main>
     <v-container class="fill-height">
-      <v-row>
-        <v-col>
-          <h1 class="font-weight-bold text-center mb-4">Bienvenue sur votre profil</h1>
+      <div v-if="!isAuthenticated" class="w-100 text-center mt-16">
+        <v-row>
+          <v-col cols="12">
+            <p class="text-center">Vous n'êtes pas connecté. <router-link to="/login" class="font-weight-bold">Cliquez ici pour vous connecter.</router-link></p>
+          </v-col>
+        </v-row>
+      </div>
 
-        </v-col>
-      </v-row>
-     
-      <v-row class="d-flex justify-center">
-        <v-col cols="12" md="5">
-          Budget
-        </v-col>
+      <div v-else class="w-100 text-center mt-16">
+        <v-row>
+          <v-col>
+            <h1 class="font-weight-bold text-center mb-16">Bienvenue sur votre profil {{ user }}.</h1>
+          </v-col>
+        </v-row>
+      
+        <v-row class="d-flex justify-center">
+          <v-col
+            v-for="card in cardsInfos"
+            :key="card.id"
+            cols="12"
+            md="6"
+          >
+            <v-card
+              :color="color"
+              class="mx-auto text-white py-8"
+              elevation="8"
+            >
+              <v-card-item>
+                <div>
+                  <v-icon class="text-h3 me-3 mb-3">{{ card.icon }}</v-icon>
 
-        <v-col cols="12" md="5">
-          Rentrées d'argent
-        </v-col>
+                  <div>
+                    <div class="text-h6 font-weight-bold">{{ card.name }}</div>
+                    <div class="text-caption">{{ card.description }}</div>
+                  </div>
+                </div>
+              </v-card-item>
 
-        <v-col cols="12" md="5">
-          Dépenses
-        </v-col>
-      </v-row>
+              <v-card-actions class="d-flex justify-center">
+                <RouterLink :to="card.link" class="routerLink">
+                  <v-btn variant="outlined" color="white" text="Accéder"></v-btn>
+                </RouterLink>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
     </v-container>
   </main>
 </template>
@@ -28,4 +55,55 @@
 
 <script setup>
 
+import { ref } from 'vue'
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+
+const user = authStore.user;
+const isAuthenticated = authStore.isAuthenticated;
+
+
+// const names = ['incomes', 'expenses', 'categories', 'budgets'];
+const color = ref('grey-darken-4');
+
+
+const cardsInfos = [
+  {
+    id: 0,
+    name: "Revenus",
+    description: "Consultez et suivez vos sources de revenus mensuels.",
+    link: "/incomes",
+    icon: "mdi-cash-plus",
+  },
+  {
+    id: 1,
+    name: "Dépenses",
+    description: "Analysez et gérez vos sorties d'argent.",
+    link: "/expenses",
+    icon: "mdi-cash-minus",
+  },
+  {
+    id: 2,
+    name: "Budget",
+    description: "Créez des budgets pour mieux planifier.",
+    link: "/budgets",
+    icon: "mdi-account-cash",
+  },
+  {
+    id: 3,
+    name: "Catégories",
+    description: "Organisez vos transactions par catégorie.",
+    link: "/categories",
+    icon: "mdi-view-list",
+  },
+];
+
 </script>
+
+<style>
+.routerLink {
+  color: white;
+  text-decoration: none;
+}
+</style>
