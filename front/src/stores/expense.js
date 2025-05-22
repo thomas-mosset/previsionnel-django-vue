3d562be7bf1c2db5062d0f86db63343b53a89863
Thomas <mosset.thomas@gmail.com>
2025-05-22 16:18:12 +0200

    front - add an expense

--- front/src/stores/expense.js
+++ front/src/stores/expense.js
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import axiosAPI from '@/services/axiosInstance';
+import { useAuthStore } from "./auth";
 
 export const useExpenseStore = defineStore('expense', {
     state: () => ({
@@ -20,6 +21,38 @@ export const useExpenseStore = defineStore('expense', {
                 this.error = error.response?.data?.detail || 'Erreur de chargement';
             } finally {
                 this.loading = false;
+            }
+        },
+        async addExpense(expenseCategory, expenseAmount, expenseDate, expenseDescription) {
+            try {
+                const authStore = useAuthStore();
+                let token = authStore.token;
+                const refreshToken = authStore.refreshToken;
+
+                if(!token) {
+                    if(refreshToken) {
+                        await authStore.refreshAccessToken();
+                        token = authStore.token;
+                    } else {
+                        throw new Error('Token non trouvé. Veuillez vous reconnecter.');
+                    }
+                }
+
+                const response = await axiosAPI.post('/expenses/', {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                    category: expenseCategory,
+                    amount: expenseAmount,
+                    date: expenseDate,
+                    description: expenseDescription,
+                });
+
+                this.expenses.push(response.data);
+
+            } catch (error) {
+                console.error('Erreur lors de l\'ajout de la dépense', error);
+                throw new Error('L\'ajout de la dépense a échoué.');  
             }
         },
     },
--- front/src/views/ExpensesView.vue
+++ front/src/views/ExpensesView.vue
@@ -16,6 +16,70 @@
                     </v-col>
                 </v-row>
 
+                <!-- ADD FORM -->
+                <v-row v-if="!expenseStore.loading">
+                    <v-col cols="12">
+                        <h2 class="mb-6">Ajouter une dépense</h2>
+
+                        <v-form ref="formRef" v-model="valid" class="mb-6 d-flex align-center" @submit.prevent="handleSubmit">
+                            <v-row>
+                                <v-col cols="12" md="6">
+                                    <v-text-field
+                                        class="mx-1"
+                                        v-model="description"
+                                        label="Description"
+                                    ></v-text-field>
+                                </v-col>
+
+                                <v-col cols="12" md="6">
+                                    <v-select
+                                        class="mx-1"
+                                        label="Catégorie"
+                                        v-model="category"
+                                        :items="incomeTypeCategories"
+                                        item-title="name"
+                                        item-value="id"
+                                        :rules="[rules.required]"
+                                        no-data-text="Aucune donnée disponible"
+                                    ></v-select>
+                                </v-col>
+
+                                <v-col cols="12" md="6">
+                                    <v-number-input
+                                        class="mx-1"
+                                        label="Montant"
+                                        :min="1"
+                                        v-model="amount"
+                                        :rules="[rules.required]"
+                                    ></v-number-input>
+                                </v-col>
+   
+                                <v-col cols="12" md="6">
+                                    <v-text-field
+                                        class="mx-1"
+                                        v-model="date"
+                                        label="Date"
+                                        type="date"
+                                        :rules="[rules.required]"
+                                    ></v-text-field>
+                                </v-col>
+
+                                <v-col cols="12">
+                                    <v-btn
+                                        class="mx-1"
+                                        type="submit"
+                                        variant="tonal"
+                                        color="grey-darken-4"
+                                        :disabled="!valid"
+                                    >
+                                        Ajouter
+                                    </v-btn>
+                                </v-col>
+                            </v-row>
+                        </v-form>
+                    </v-col>
+                </v-row>
+
                 <v-row>
                     <div v-if="expenseStore.loading" class="w-100 text-center">
                         <p>Chargement...</p>
@@ -82,6 +146,18 @@
                     </v-data-table>
                 </v-row>
 
+                <v-snackbar
+                    v-model="snackbar"
+                    :timeout="3000"
+                    :color="snackbarColor"
+                    elevation="8"
+                    location="top"
+                >
+                    <v-row class="justify-center">
+                    <span class="text-center">{{ snackbarMessage }}</span>
+                    </v-row>
+                </v-snackbar>
+
             </div>
         </v-container>
     </main>
@@ -97,8 +173,26 @@ const authStore = useAuthStore();
 const isAuthenticated = authStore.isAuthenticated;
 
 const categoryStore = useCategoryStore();
+const categories = computed(() => categoryStore.categories);
 
 const expenseStore = useExpenseStore();
+
+const snackbar = ref(false); // vuetify element
+const snackbarMessage = ref(''); // vuetify element
+const snackbarColor = ref('');
+
+// add form
+const valid = ref(false)
+const error = ref(null)
+const date = ref('');
+const description = ref(null);
+const category = ref('');
+const amount = ref();
+
+// validation add form
+const rules = {
+  required: (v) => !!v || 'Champ requis',
+}
 
 onMounted(() => {
     authStore.initialize();
@@ -136,5 +230,40 @@ const displayedExpensesWithTextCategories = computed(() => {
         };
     });
 });
+
+const incomeTypeCategories = computed(() => {
+    const rawCategories = categories.value;
+
+    return Array.isArray(rawCategories)
+    ? rawCategories.filter(category => category.type === 'EXPENSE')
+    : [];
+});
+
+const handleSubmit = async () => {
+    error.value = null
+
+    try {
+        await expenseStore.addExpense(category.value, amount.value, date.value, description.value);
+
+        snackbarMessage.value = 'Dépense ajoutée !';
+        snackbarColor.value = 'green-darken-4';
+        snackbar.value = true;
+
+        // Reset form
+        setTimeout(() => {
+            date.value = '';
+            description.value = null;
+            category.value = '';
+            amount.value = null;
+        }, 3000);
+
+    } catch (error) {
+        console.error('Erreur lors de l\'ajout de la dépense', error);
+
+        snackbarMessage.value = "Erreur lors de l'ajout.";
+        snackbarColor.value = 'deep-orange-accent-4';
+        snackbar.value = true;
+    }
+};
 
 </script>
