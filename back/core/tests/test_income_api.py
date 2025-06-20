1961a4ba3380bae6fa05f146b91f7f3caaad36b4
Thomas <mosset.thomas@gmail.com>
2025-06-20 16:32:08 +0200

    back tests - CRUD integration test of income API

--- back/core/tests/test_income_api.py
+++ back/core/tests/test_income_api.py
@@ -41,3 +41,38 @@ class IncomeAPITest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]['description'], "Salaire de juin")
+    
+    def test_update_income(self):
+        # create quickly a category + an income ...
+        category = Category.objects.create(name='Salaire', type='INCOME', user=self.user)
+
+        income = Income.objects.create(category=category, amount=2000, date="2025-06-20", description="Salaire de juin", user=self.user)
+
+        # generate "/api/incomes/<id>/"
+        url = reverse('income-detail', args=[income.id]) # [income.id] = id of our object
+        data = {
+            "amount": 2500,
+            "description": "Salaire de juin mis à jour"
+        }
+
+        # "patch" because we only update our data partially (else we would have user "put")
+        response = self.client.patch(url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        income.refresh_from_db() # reload from the database to check for changes
+        self.assertEqual(income.amount, 2500)
+        self.assertEqual(income.description, "Salaire de juin mis à jour")
+
+    def test_delete_income(self):
+        # create quickly a category + an income ...
+        category = Category.objects.create(name='Salaire', type='INCOME', user=self.user)
+
+        income = Income.objects.create(category=category, amount=2000, date="2025-06-20", description="Salaire de juin", user=self.user)
+
+        # create url
+        url = reverse('income-detail', args=[income.id])
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) # delete ok 
+        self.assertFalse(Income.objects.filter(id=income.id).exists()) # check that object is gone
